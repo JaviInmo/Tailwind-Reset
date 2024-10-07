8f379d29f54e7744b3fc72de2c44e2f50711d374
Javi Estevez <inmolationsc2@gmail.com>
2024-10-07 11:28:01 -0400

    asd

--- tailwindlast_admin/tailwind.config.ts
+++ tailwindlast_admin/tailwind.config.ts
@@ -11,9 +11,22 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        slate: {
+          50: "#f8fafc",
+          100: "#f1f5f9",
+          200: "#e2e8f0",
+          300: "#cbd5e1",
+          400: "#94a3b8",
+          500: "#64748b",
+          600: "#475569",
+          700: "#334155",
+          800: "#1e293b",
+          900: "#0f172a",
+        },
       },
     },
   },
   plugins: [],
 };
+
 export default config;
