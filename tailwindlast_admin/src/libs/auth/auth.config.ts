d656204a0e9811ebee29eb6661a0ca9b46e9fecd
Javi Estevez <inmolationsc2@gmail.com>
2024-10-10 12:14:49 -0400

    revert authconfigts

--- tailwindlast_admin/src/libs/auth/auth.config.ts
+++ tailwindlast_admin/src/libs/auth/auth.config.ts
@@ -1,17 +1,15 @@
-import { AuthOptions, User } from "next-auth";
+import { NextAuthConfig, User } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { z } from "zod";
 
 import prisma from "../db";
 
-// Definir el esquema de las credenciales
 const credentialsSchema = z.object({
   name: z.string(),
   password: z.string(),
 });
 
-// Configuración de NextAuth
-export const authConfig: AuthOptions = {
+export const authConfig = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
@@ -20,36 +18,27 @@ export const authConfig: AuthOptions = {
         password: { label: "Password", type: "password" },
       },
       authorize: async (credentials): Promise<User | null> => {
-        // Validar las credenciales usando Zod
         const { name, password } = credentialsSchema.parse(credentials);
 
         try {
-          // Buscar el usuario en la base de datos usando Prisma
           const user = await prisma.user.findFirstOrThrow({
             where: { name, password },
           });
-          // Devolver el usuario si la autenticación es exitosa
           return { name: user.name, image: user.image, id: user.id };
         } catch (e) {
-          // Devolver null si hay algún error (usuario no encontrado)
           return null;
         }
       },
     }),
   ],
   callbacks: {
-    async signIn({ user }) {
-      // Si el usuario existe, permitir el acceso
-      if (user) {
-        return true;
-      }
-      // De lo contrario, negar el acceso
-      return false;
-    },
-    async session({ session, user }) {
-      // Agregar los detalles del usuario a la sesión
-      session.user = user;
-      return session;
+    authorized({ request, auth }) {
+      const { pathname } = request.nextUrl;
+      console.log("[pathname] ", pathname);
+      console.log("[auth] ", auth);
+      if (pathname.includes("/admin")) return !!auth;
+
+      return true;
     },
   },
-};
+} satisfies NextAuthConfig;
