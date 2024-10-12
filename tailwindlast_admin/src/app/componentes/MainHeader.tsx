7aa5a36a004ba8b4cd3b0e72ed541e4d15a97a3e
Javi Estevez <inmolationsc2@gmail.com>
2024-10-12 17:37:08 -0400

    final layout sketch

--- tailwindlast_admin/src/app/componentes/MainHeader.tsx
+++ tailwindlast_admin/src/app/componentes/MainHeader.tsx
@@ -1,17 +1,31 @@
 "use client";
 import React, { useContext } from "react";
 import { FaBars } from "react-icons/fa";
-import MenuContextProvider, { MenuContext } from "../context/MenuContext";
+import { CgProfile } from "react-icons/cg";
+import { MdLogout } from "react-icons/md";
+import { MenuContext } from "../context/MenuContext";
 
 const MainHeader = () => {
   const { toggle } = useContext(MenuContext);
+
   return (
-    <div className="flex justify-between items-center px-2 h-12 mb-0 gap-2">
-      <div onClick={toggle} className="lg:hidden">
-        <FaBars className="cursor-pointer" />
+    <div className=" flex justify-between items-center px-2 py-2 h-9 w-full mb-0 gap-2 ">
+      <div>
+        <div onClick={toggle} className="lg:hidden">
+          <FaBars className="cursor-pointer" />
+        </div>
+        <div>MainHeaderBrand</div>
       </div>
-      <div>MainHeaderBrand</div>
+      <div className="flex justify-between items-center px-2 py-2 gap-2  ">
+        <div className="flex justify-between items-center px-2 py-2 gap-2  hover:bg-slate-700 hover:underline rounded-xl text-2xl ">
+          <CgProfile />
+        </div>
+        <div className="flex justify-between items-center px-2 py-2 gap-2  hover:bg-slate-700 hover:underline rounded-xl text-2xl">
+          <MdLogout />
+        </div>
+      </div>
     </div>
   );
 };
+
 export default MainHeader;
--- tailwindlast_admin/src/app/componentes/MainLayout.tsx
+++ tailwindlast_admin/src/app/componentes/MainLayout.tsx
@@ -8,29 +8,36 @@ import { BsFileText } from "react-icons/bs";
 import { VscGraphLine } from "react-icons/vsc";
 import { GoTable } from "react-icons/go";
 import { VscVariableGroup } from "react-icons/vsc";
-import { FaAngleRight } from "react-icons/fa6";
+import { FaAngleRight, FaAnglesLeft } from "react-icons/fa6";
 import { RiAdminLine } from "react-icons/ri";
 import Link from "next/link";
 import { MenuContext } from "../context/MenuContext"; // Asegúrate de que esta ruta es correcta
 
 const MainLayout = ({ children }: PropsWithChildren) => {
-  const { open } = useContext(MenuContext);
+  const { open, toggle } = useContext(MenuContext); // Añadimos toggle para manejar el estado
 
   return (
     <div className="bg-slate-100 w-screen min-h-screen relative flex">
       <aside
-        className={`bg-slate-800 text-slate-100 absolute lg:fixed top-0 left-0 h-full transition-transform duration-500 ease-in-out transform ${
+        className={`bg-slate-800 text-slate-100 fixed top-0 left-0 h-full transition-transform duration-500 ease-in-out z-50 ${
           open ? "translate-x-0" : "-translate-x-full"
-        } lg:translate-x-0 z-50`}
+        } lg:translate-x-0`}
         style={{
-          width: "15rem",
+          width: "15rem", // Mantener el ancho fijo
           overflow: "hidden",
         }}
       >
-        <ul className="transition-opacity duration-500 ease-in-out py-2 px-2 mr-4">
-          <li className="flex justify-start items-center hover:bg-slate-700 hover:underline rounded-xl p-2 mb-6">
-            <AiOutlineHome className="mr-2" />
-            <Link href="/">Home</Link>
+        <ul className="py-2 px-2 mr-4">
+          <li className="flex justify-start items-center hover:bg-slate-700 hover:underline rounded-xl p-2 mb-6  ">
+            <AiOutlineHome className="mr-2 font-bold text-xl" />
+            <Link className="flex-1 font-bold text-xl" href="/">
+              Home
+            </Link>
+            {/* Mostrar el icono solo fuera de lg */}
+            <FaAnglesLeft
+              className="block lg:hidden cursor-pointer"
+              onClick={toggle} // Cerrar el menú cuando se haga clic en la flecha
+            />
           </li>
           <li className="flex justify-start items-center hover:bg-slate-700 hover:underline rounded-xl p-2">
             <RxDashboard className="mr-2" />
@@ -60,20 +67,18 @@ const MainLayout = ({ children }: PropsWithChildren) => {
           </li>
         </ul>
       </aside>
+
       <div
         className={`flex flex-col flex-1 transition-all duration-500 ease-in-out ${
-          open ? "ml-60" : "ml-0"
+          open ? "opacity-80" : "opacity-100"
         } lg:ml-60`}
       >
         <header className="flex-none w-full bg-slate-900 text-white h-16 flex items-center px-4 transition-all duration-500 ease-in-out">
           <MainHeader />
         </header>
-        <main className="flex-1 bg-slate-50 p-4 transition-all duration-500 ease-in-out">
+        <main className="flex-1 bg-slate-200 p-4 transition-all duration-500 ease-in-out">
           <div className="bg-white shadow-md p-6 rounded-lg">{children}</div>
         </main>
-        <footer className="flex-none w-full bg-slate-700 text-slate-200 h-16 flex items-center justify-center transition-all duration-500 ease-in-out">
-          Footer Content
-        </footer>
       </div>
     </div>
   );
