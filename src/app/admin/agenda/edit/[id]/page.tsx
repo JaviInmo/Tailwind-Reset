a943c44ff40050ba7d30c11d6ae8e0354e51dafe
oljimenez <oljimenez.prog@gmail.com>
2025-04-17 00:40:42 -0400

    fix: table refresh

--- src/app/admin/agenda/edit/[id]/page.tsx
+++ src/app/admin/agenda/edit/[id]/page.tsx
@@ -1,36 +1,32 @@
 import { ContactForm } from "@/app/admin/agenda/create/contact-form";
 import { getAuth } from "@/libs/auth";
 import prisma from "@/libs/db";
+import { notFound } from "next/navigation";
 
-export default async function FormPage(props: { params: Promise<{ id: string }> }) {
-    const params = await props.params;
-    await getAuth();
+export default async function FormPage(props: {
+	params: Promise<{ id: string }>;
+}) {
+	const params = await props.params;
+	await getAuth();
 
-    return (
-        <EditContactPage params={params} />
-    );
+	return <EditContactPage params={params} />;
 }
 
-export async function EditContactPage({ params }: { params: { id: string } }){
-    const id = Number(params.id); 
+export async function EditContactPage({ params }: { params: { id: string } }) {
+	const id = Number(params.id);
 
-    const contactData = await prisma.contact.findUnique({
-        where: { id },
-        include: { province: true, municipality: true },
-    });
+	const contactData = await prisma.contact.findUnique({
+		where: { id },
+		include: { province: true, municipality: true },
+	});
 
-    if (!contactData) {
-        return { notFound: true };
-    }
+	if (!contactData) {
+		notFound();
+	}
 
-    const provinceData = await prisma.province.findMany({
-        include: { municipalities: true },
-    });
+	const provinceData = await prisma.province.findMany({
+		include: { municipalities: true },
+	});
 
-    return (
-        <ContactForm
-            contactData={contactData}
-            provinceData={provinceData}
-        />
-    );
+	return <ContactForm contactData={contactData} provinceData={provinceData} />;
 }
